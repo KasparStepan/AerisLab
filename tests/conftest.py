aa689ade2cc01147512b05068393fb63ff4a1d87
KasparStepan <xkaspa43@vutbr.cz>
2026-01-05 13:12:11 +0100

    conftest deletion

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-
-# Add the project root directory to sys.path to allow importing AerisLab
-# This ensures tests can run even if the package is not installed in the environment
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
