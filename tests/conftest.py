e83b4fb00202e691f85ba40635753ecee26dacb9
KasparStepan <xkaspa43@vutbr.cz>
2026-01-05 13:22:32 +0100

    conftest addition

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+# Get the path to the project root (one level up from 'tests')
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+src_path = os.path.join(project_root, 'src')
+
+# Add 'src' to sys.path
+sys.path.insert(0, src_path)
